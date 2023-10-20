5b9ddd0d742b1e9e2705d745934574874a9d0c8a
James Polley <jp@jamezpolley.com>
2023-10-20 12:21:47 +1100

    Update scraper.rb

--- scraper.rb
+++ scraper.rb
@@ -7,6 +7,10 @@ agent = Mechanize.new
 # This endpoint is not "protected" by Kasada
 url = "https://cdn.plan.sa.gov.au/public-notifications/getpublicnoticessummary"
 response = agent.get(url)
+rescue Mechanize::ResponseCodeError => errorResponse
+  puts "Encountered error code #{errorResponse#response_code}"
+  puts "Body:\n#{errorResponse#page}
+end
 applications = JSON.parse(response.body)
 applications.each do |application|
   record = {
